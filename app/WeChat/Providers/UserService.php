a7ae811a0cf8ec7bfeceb13ac397e345045f766a
macro <macro_fengye@163.com>
2016-10-11 15:29:12 +0800

    清理代码

--- app/WeChat/Providers/UserService.php
+++ app/WeChat/Providers/UserService.php
@@ -1,28 +1,4 @@
 <?php
-
-/*
- * This file is part of the overtrue/wechat.
- *
- * (c) overtrue <i@overtrue.me>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
-/**
- * UserServiceProvider.php.
- *
- * Part of Overtrue\WeChat.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @author    overtrue <i@overtrue.me>
- * @copyright 2015
- *
- * @link      https://github.com/overtrue/wechat
- * @link      http://overtrue.me
- */
 namespace WeChat\Providers;
 
 use WeChat\Utils\WeChat\User\Group;
